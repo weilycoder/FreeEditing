d9a109855e0e95fc52768d7343c28b18455022b8
weilycoder <weily2009@outlook.com>
2024-05-12 23:04:25 +0800

    first commit

---
+++ code.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         自由编辑
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-12
+// @description  尝试称霸世界！我是说，这个脚本可以让你编辑网页内容
+// @author       weilycoder
+// @match        *://*/*
+// @icon         https://img2.imgtp.com/2024/05/12/Jjejy7fj.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function checkIfCtrlPressed(event) {
+        if (event.ctrlKey == 1) {
+            if (document.body.contentEditable!=='true') {
+                document.body.contentEditable='true';
+                alert("已解锁限制");
+            } else {
+                document.body.contentEditable='inherit';
+                alert("已打开限制");
+            }
+        }
+    }
+    document.body.onmousedown=checkIfCtrlPressed;
+})();
